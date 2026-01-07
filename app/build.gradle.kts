383375311ecf3174351839e757c15a935b797c3c
JiY <jiyong3954@gmail.com>
2026-01-07 23:48:13 +0900

    di: 프로젝트 hilt 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.kotlin.ksp)
+  alias(libs.plugins.android.hilt)
 }
 
 android {
@@ -58,4 +60,7 @@ dependencies {
   androidTestImplementation(libs.androidx.compose.ui.test.junit4)
   debugImplementation(libs.androidx.compose.ui.tooling)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+  implementation(libs.hilt.android)
+  ksp(libs.hilt.compiler)
 }
--- app/src/main/java/com/dd2d/now_in_my_android/MainActivity.kt
+++ app/src/main/java/com/dd2d/now_in_my_android/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dd2d.now_in_my_android.ui.theme.NowInMyAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
