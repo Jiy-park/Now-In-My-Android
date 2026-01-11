c4816a0d8b8649379a8a91a8c73208757d083965
JiY <jiyong3954@gmail.com>
2026-01-11 14:50:45 +0900

    refactoring(json-placeholder): `Flow.stateIn` -> `Flow.stateInXXX` StateFlow 변환 방식 개선

--- feat/json-placeholder/src/main/java/com/dd2d/json_placeholder/post/presentation/list/PostListViewModel.kt
+++ feat/json-placeholder/src/main/java/com/dd2d/json_placeholder/post/presentation/list/PostListViewModel.kt
@@ -2,14 +2,13 @@ package com.dd2d.json_placeholder.post.presentation.list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dd2d.core.flow.stateInWhileSubscribed
 import com.dd2d.core.stateful.Stateful
 import com.dd2d.core.stateful.statefulFlow
 import com.dd2d.json_placeholder.post.domain.PostRepository
 import com.dd2d.json_placeholder.post.domain.model.Post
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,9 +16,5 @@ class PostListViewModel @Inject constructor(
   postRepository: PostRepository
 ) : ViewModel() {
   val postListState: StateFlow<Stateful<List<Post>>> = statefulFlow { postRepository.getPostList() }
-    .stateIn(
-      scope = viewModelScope,
-      started = SharingStarted.WhileSubscribed(5000),
-      initialValue = Stateful.Loading
-    )
+    .stateInWhileSubscribed(scope = viewModelScope, initialValue = Stateful.Loading)
 }
--- feat/json-placeholder/src/main/java/com/dd2d/json_placeholder/user/presentation/detail/UserDetailViewModel.kt
+++ feat/json-placeholder/src/main/java/com/dd2d/json_placeholder/user/presentation/detail/UserDetailViewModel.kt
@@ -4,17 +4,16 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
+import com.dd2d.core.flow.stateInWhileSubscribed
 import com.dd2d.core.stateful.Stateful
+import com.dd2d.core.stateful.mapSuccessState
 import com.dd2d.core.stateful.statefulFlow
 import com.dd2d.json_placeholder.user.domain.UserRepository
 import com.dd2d.json_placeholder.user.presentation.detail.model.TodoListUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.scan
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,7 +24,7 @@ class UserDetailViewModel @Inject constructor(
   val userDetailScreenRoute = savedStateHandle.toRoute<UserDetailScreenRoute>()
 
   val userDetailState = statefulFlow { userRepository.getUser(userDetailScreenRoute.userId) }
-    .stateIn(scope = viewModelScope, started = SharingStarted.WhileSubscribed(5000), initialValue = Stateful.Loading)
+    .stateInWhileSubscribed(scope = viewModelScope, initialValue = Stateful.Loading)
 
 
   private val todoListCompleteFilter = MutableStateFlow<Boolean?>(null)
@@ -33,23 +32,15 @@ class UserDetailViewModel @Inject constructor(
     .flatMapLatest { completeFilter ->
       statefulFlow { userRepository.getTodoList(userDetailScreenRoute.userId, completeFilter) }
     }
-    .map { state ->
-      when(state) {
-        is Stateful.Loading -> Stateful.Loading
-        is Stateful.Error -> Stateful.Error(state.exception)
-        is Stateful.Success -> {
-          Stateful.Success(
-            data = TodoListUIState(
-              todoList = state.data,
-              completeFilter = todoListCompleteFilter.value,
-              onFilterChange = { todoListCompleteFilter.value = it },
-            )
-          )
-        }
-      }
+    .mapSuccessState { todoList ->
+      TodoListUIState(
+        todoList = todoList,
+        completeFilter = todoListCompleteFilter.value,
+        onFilterChange = { todoListCompleteFilter.value = it },
+      )
     }
     .scan(initial = Stateful.Loading) { old: Stateful<TodoListUIState>, new ->
       (new as? Stateful.Success)?: old
     }
-    .stateIn(scope = viewModelScope, started = SharingStarted.WhileSubscribed(5000), initialValue = Stateful.Loading)
+    .stateInWhileSubscribed(scope = viewModelScope, initialValue = Stateful.Loading)
 }
