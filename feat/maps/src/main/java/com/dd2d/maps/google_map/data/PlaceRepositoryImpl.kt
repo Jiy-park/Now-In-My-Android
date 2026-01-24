fb7355d398aa9d2d143e0427b402b1b19e13193a
JiY <jiyong3954@gmail.com>
2026-01-24 18:53:40 +0900

    feat(maps): data 레이어 추가
    - PlaceRepositoryImpl 추가
    - Hilt 모듈 적용

---
+++ feat/maps/src/main/java/com/dd2d/maps/google_map/data/PlaceRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.dd2d.maps.google_map.data
+
+import com.dd2d.maps.google_map.data._source.local.LocationSource
+import com.dd2d.maps.google_map.data._source.remote.GooglePlaceApi
+import com.dd2d.maps.google_map.domain.PlaceRepository
+import com.dd2d.maps.google_map.domain.model.Place
+import com.dd2d.maps.google_map.domain.model.PlaceImage
+import com.dd2d.maps.google_map.domain.model.PlaceLocation
+import kotlinx.coroutines.CancellationException
+import javax.inject.Inject
+
+internal class PlaceRepositoryImpl @Inject constructor(
+  private val locationSource: LocationSource,
+  private val googlePlaceApi: GooglePlaceApi,
+): PlaceRepository {
+  override suspend fun getCurrentLocation(): PlaceLocation {
+    val location = locationSource.getCurrentLocation()
+    return PlaceLocation(
+      latitude = location.latitude,
+      longitude = location.longitude
+    )
+  }
+
+  override suspend fun getPlace(id: String): Place {
+    val response =
+      try { googlePlaceApi.getPlace(placeId = id) }
+      catch (exception: CancellationException) { throw exception }
+
+    return Place(
+      id = response.id,
+      name = response.displayName?.text,
+      fullAddress = response.formattedAddress,
+      location = response.location?.let { dto ->
+        PlaceLocation(dto.latitude, dto.longitude)
+      },
+      types = response.types?: emptyList(),
+      contact = response.internationalPhoneNumber,
+      rating = response.rating,
+      userRatingCount = response.userRatingCount,
+      googleMapUri = response.googleMapsUri,
+      websiteUrl = response.websiteUri,
+      openNow = response.currentOpeningHours?.openNow,
+      weekdayDescriptions = response.currentOpeningHours?.weekdayDescriptions,
+      images = response.photos?.map { photo ->
+        PlaceImage(
+          placeId = response.id,
+          name = photo.name,
+          originWidth = photo.widthPx,
+          originHeight = photo.heightPx,
+        )
+      }?: emptyList(),
+    )
+  }
+}
