92dbf97bf8b6384f358dedb394763638a8774ec2
JiY <jiyong3954@gmail.com>
2026-01-29 23:05:39 +0900

    feat(maps): `GooglePlaceApi.searchByText` 추가
    - 검색어(`textQuery`)로 장소를 검색하는 api를 추가했습니다.
    - `locationBias`를 통해 특정 위치를 우선적으로 검색할 수 있습니다.

--- feat/maps/src/main/java/com/dd2d/maps/google_map/data/_source/remote/GooglePlaceApi.kt
+++ feat/maps/src/main/java/com/dd2d/maps/google_map/data/_source/remote/GooglePlaceApi.kt
@@ -1,15 +1,19 @@
 package com.dd2d.maps.google_map.data._source.remote
 
+import com.dd2d.maps.google_map.data._source.remote.dto.request.GooglePlaceSearchByTextRequestDto
 import com.dd2d.maps.google_map.data._source.remote.dto.response.GooglePlaceResponseDto
+import com.dd2d.maps.google_map.data._source.remote.dto.response.GooglePlaceSearchPlaceListItemResponseDto
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 internal interface GooglePlaceApi {
-  @GET("places/{placeId}")
   companion object {
     const val PLACE_FIELD_MASK = "id,displayName,location,formattedAddress,types,businessStatus,rating,userRatingCount,priceLevel,regularOpeningHours,currentOpeningHours,internationalPhoneNumber,websiteUri,googleMapsUri,photos"
+    const val SEARCH_BY_TEXT_FIELD_MASK = "places.id,places.displayName,places.formattedAddress,nextPageToken"
   }
 
   @GET("v1/places/{placeId}")
@@ -18,4 +22,11 @@ internal interface GooglePlaceApi {
     @Query("languageCode") languageCode: String = "ko-KR",
     @Header("X-Goog-FieldMask") fieldMask: String = PLACE_FIELD_MASK
   ): GooglePlaceResponseDto
+
+  @POST("v1/places:searchText")
+  suspend fun searchByText(
+    @Body body: GooglePlaceSearchByTextRequestDto,
+    @Query("languageCode") languageCode: String = "ko",
+    @Header("X-Goog-FieldMask") fieldMask: String = SEARCH_BY_TEXT_FIELD_MASK,
+  ): GooglePlaceSearchPlaceListItemResponseDto
 }
---
+++ feat/maps/src/main/java/com/dd2d/maps/google_map/data/_source/remote/dto/request/GooglePlaceSearchByTextRequestDto.kt
@@ -0,0 +1,26 @@
+package com.dd2d.maps.google_map.data._source.remote.dto.request
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class GooglePlaceSearchByTextRequestDto(
+  val textQuery: String,
+  val locationBias: PlaceSearchLocationBias?,
+)
+
+@Serializable
+internal data class PlaceSearchLocationBias(
+  val circle: PlaceSearchLocationBiasCircle,
+)
+
+@Serializable
+internal data class PlaceSearchLocationBiasCircle(
+  val center: PlaceSearchLocationBiasCircleCenter,
+  val radius: Int,
+)
+
+@Serializable
+internal data class PlaceSearchLocationBiasCircleCenter(
+  val latitude: Double,
+  val longitude: Double,
+)
