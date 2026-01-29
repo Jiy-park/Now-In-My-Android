745a8c99c1ccccc23e70df5db277e83be0bbc955
JiY <jiyong3954@gmail.com>
2026-01-29 23:02:24 +0900

    refactor(maps): `GooglePlaceApi.getPlace`의 `fieldMask` 필드 기본값을 상수(`PLACE_FIELD_MASK`)로 추출

--- feat/maps/src/main/java/com/dd2d/maps/google_map/data/_source/remote/GooglePlaceApi.kt
+++ feat/maps/src/main/java/com/dd2d/maps/google_map/data/_source/remote/GooglePlaceApi.kt
@@ -8,11 +8,14 @@ import retrofit2.http.Query
 
 internal interface GooglePlaceApi {
   @GET("places/{placeId}")
+  companion object {
+    const val PLACE_FIELD_MASK = "id,displayName,location,formattedAddress,types,businessStatus,rating,userRatingCount,priceLevel,regularOpeningHours,currentOpeningHours,internationalPhoneNumber,websiteUri,googleMapsUri,photos"
+  }
+
   @GET("v1/places/{placeId}")
   suspend fun getPlace(
     @Path("placeId") placeId: String,
     @Query("languageCode") languageCode: String = "ko-KR",
-    @Header("X-Goog-FieldMask") fieldMask: String =
-      "id,displayName,location,formattedAddress,types,businessStatus,rating,userRatingCount,priceLevel,regularOpeningHours,currentOpeningHours,internationalPhoneNumber,websiteUri,googleMapsUri,photos"
+    @Header("X-Goog-FieldMask") fieldMask: String = PLACE_FIELD_MASK
   ): GooglePlaceResponseDto
 }
